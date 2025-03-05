334879c5d974480b7dc0c93c8f850cf7be533322
imkawe <zebimkawed@gmail.com>
2025-03-05 01:32:27 -0500

    Ignorando carpeta dist

--- dist/assets/NotFound-DleiOIBt.js
+++
@@ -1,1 +0,0 @@
-import{j as o,L as n}from"./index-BmcAolYd.js";const e=()=>o.jsxs("div",{style:{textAlign:"center",marginTop:"50px"},children:[o.jsx("h1",{children:"404 - Page Not Found"}),o.jsx("p",{children:"Sorry, the page you are looking for does not exist."}),o.jsx(n,{to:"/login",children:"Go to Login"}),o.jsx("br",{}),o.jsx(n,{to:"/signup",children:"Go to Signup"})]});export{e as default};

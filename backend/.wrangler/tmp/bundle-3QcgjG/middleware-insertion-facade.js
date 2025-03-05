92ef62025a4a51c0abbe44c5c2bb27fc90290b66
imkawe <zebimkawed@gmail.com>
2025-03-05 01:02:13 -0500

    Corrigiendo conflictos y sincronizando con main

---
+++ backend/.wrangler/tmp/bundle-3QcgjG/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\emy\\oneproduct\\backend\\src\\index.js";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\emy\\AppData\\Local\\pnpm\\global\\5\\.pnpm\\wrangler@3.108.1\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "C:\\Users\\emy\\AppData\\Local\\pnpm\\global\\5\\.pnpm\\wrangler@3.108.1\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "C:\\Users\\emy\\oneproduct\\backend\\src\\index.js";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;

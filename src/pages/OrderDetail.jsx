5bddf3a069c3dea365f567fdfa98745c1cd1f72e
imkawe <zebimkawed@gmail.com>
2025-03-06 16:56:15 -0500

    date

--- src/pages/OrderDetail.jsx
+++ src/pages/OrderDetail.jsx
@@ -8,9 +8,8 @@ const OrderDetail = ({ order, onClose }) => {
     const options = { 
       year: 'numeric', 
       month: 'long', 
-      day: 'numeric', 
-      hour: '2-digit', 
-      minute: '2-digit' 
+      day: 'numeric' 
+    
     };
     return new Date(dateString).toLocaleDateString('es-ES', options);
   };
